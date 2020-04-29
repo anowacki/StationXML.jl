9558444632cc37aff7a72fa511ab2f1a56baeade
Andy Nowacki <a.nowacki@leeds.ac.uk>
2020-04-29 12:42:11 +0100

    Add missing JSA.xml file and add raw Gzipped reader to tests

--- test/test_util.jl
+++ test/test_util.jl
@@ -7,6 +7,7 @@ using CodecZlib: GzipDecompressorStream
 datapath(file) = joinpath(@__DIR__, "data", file)
 
 "Read a Gzipped example file"
-gzipped_read(file) = open(datapath(file)) do io
-    StationXML.readstring(read(GzipDecompressorStream(io), String))
-end
+gzipped_read(file) = StationXML.readstring(String(gzipped_read_raw(file)))
+
+"Read a Gzipped example file as a raw set of bytes"
+gzipped_read_raw(file) = open(io -> read(GzipDecompressorStream(io)), datapath(file))
